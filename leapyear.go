d0387b87a6ce97255fce49219f53ed8d9886b788
raushan211 <raazcrzy90@gmail.com>
2022-06-13 18:04:19 +0530

    leapyear

--- leapyear.go
+++ leapyear.go
@@ -7,7 +7,7 @@ func main() {
 	var year int
 	fmt.Scanln(&year)
 
-	if year%400 == 0 && year%4 == 0 {
+	if year%400 == 0 || year%4 == 0 && year%100 != 0 {
 		fmt.Println("Leap Year")
 	} else {
 		fmt.Println("Not Leap Year")
